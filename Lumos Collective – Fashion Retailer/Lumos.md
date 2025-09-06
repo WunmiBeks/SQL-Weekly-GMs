42035c1fbc3e4ddc6acc5280db80267d3f388920
WunmiBeks <wunmi.cloud@gmail>
2025-09-06 09:08:55 -0600

    SQL updated

---
+++ Lumos Collective – Fashion Retailer/Lumos.md
@@ -0,0 +1,14 @@
+# Project Brief - Lumos Collective
+
+ We’re running a big 48-hour Flash Sale starting this weekend, and I need your help with the audience pull.  
+
+## Requirements
+- They must be Active customers.  
+- Target customers who either:  
+   - purchased in the last 180 days AND their preferred category is Shoes or Bags  
+   - Or they’ve been browsing recently (last browse date within the last 14 days).  
+
+Also, create a custom field in the output called SaleUrgency for the email team.
+- If someone’s last browse date was within the last 7 days → mark as Hot.  
+- If it was 8–14 days → mark as Warm.  
+- Otherwise, just mark as Cold.  
--- Lumos Collective – Fashion Retailer/Lumos.sql
+++ Lumos Collective – Fashion Retailer/Lumos.sql
@@ -9,16 +9,18 @@ SELECT      CustomerID,
             LastBrowseDate,
             CASE 
                WHEN LastBrowseDate >= DATEADD(D, -7, GETDATE()) THEN 'Hot'
-               WHEN LastBrowseDate >= DATEADD(D, -14, GETDATE()) THEN 'Warm' 
+               WHEN DATEDIFF(day, LastBrowseDate, GETDATE()) BETWEEN 8 AND 14 THEN 'Warm'
                ELSE 'Cold' 
             END AS SaleUrgency
 FROM        [Customer_Profile]
 WHERE       Status = 'Active'
 AND         Email_Consent = 'True'
 AND         (
-              ((LastPurchaseDate >= DATEADD(D, -180, GETDATE()) OR LastPurchaseDate IS NULL)
-              AND PreferredCategory IN ('Shoes', 'Bags'))
+              (
+               LastPurchaseDate >= DATEADD(D, -180, GETDATE())
+              AND PreferredCategory IN ('Shoes', 'Bags')
+              )
             OR 
-            (LastBrowseDate >= DATEADD(D, -14, GETDATE()) OR LastBrowseDate IS NULL )
-             )
+            LastBrowseDate >= DATEADD(D, -14, GETDATE()) 
+            )
             
